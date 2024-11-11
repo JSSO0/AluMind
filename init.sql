525cf002466703c2228bb69f0ea56e79f867e565
Joely Silva <sjoely131@gmail.com>
2024-11-11 11:12:39 -0300

    ajuste do SQL para ficar compátivel com os tamanhos dos valores

--- init.sql
+++ init.sql
@@ -2,7 +2,7 @@ CREATE TABLE feedback (
   id SERIAL PRIMARY KEY,
   sentiment VARCHAR(255),
   code VARCHAR(255),
-  reason VARCHAR(255),
-  feedbackOriginal VARCHAR(255),
-  suggestedAnswer VARCHAR(255)
+  reason text,
+  feedbackOriginal text,
+  suggestedAnswer text
 );
